c828967f8f033cd5088aa58cc538bc6fd7166b91
Michael Gouwens <Radiosatch2000@yahoo.com>
2024-12-20 11:40:05 -0600

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <section>
       <h2>Home</h2>
-      <p>Welcome to the recruiting pipeline!</p>
+      <p>Welcome to the Recruiting Pipeline!</p>
       <p className="spacer-lg">If this is your first time here, check out our <Link to="/analytics">Analytics</Link> page for insights on how well your recruiting is going.</p>
     </section>
       
